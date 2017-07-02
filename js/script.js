8d0aeb8968a33baa9983b4e0ce0dbdfa338bb304
ChrisYoobee <chris.hughes@hotmail.co.nz>
2017-07-02 22:05:06 +1200

    Layout 90% complete

--- js/script.js
+++ js/script.js
@@ -2,83 +2,6 @@
 
 var map;
 var marker;
-var bounce;
-var infobox;
-var userLocation;
-var clickMarker;
-var closestMarker;
-var toggleMarkerOn = true;
-var changeTravelType = "DRIVING"
-var distance;
-var currentMarker;
-var markers = [];
-var allMarkers = [
-
-	{
-		lat: -41.2820381 ,
-		lng:  174.7734063,
-		title: "Chefs Palette Courtney place" ,
-		description: "Food place"
-	},
-	{
-		lat: -41.2820367 ,
-		lng:  174.7580854,
-		title: "Burgerfuel Courtney place" ,
-		description: "Burger place"
-	},
-	{
-		lat:  -41.2928764,
-		lng: 174.7776247 ,
-		title:  "Nandos",
-		description:"Food place"
-	},
-	{
-		lat: -41.2841113 ,
-		lng: 174.7731321,
-		title:  "McDonalds",
-		description: "Food place"
-	},
-	{
-		lat:  -41.2897721,
-		lng:  174.7731366,
-		title: "Subway" ,
-		description: "Food place"
-	},
-	{
-		lat:  -41.2934607,
-		lng:  174.7830931,
-		title: "KFC" ,
-		description: "Food place"
-	},
-	{
-		lat:  -41.2934907,
-		lng:  174.7762954,
-		title: "Burger King" ,
-		description: "Food place"
-	},
-	{
-		lat:  41.2898021,
-		lng:  174.7936576,
-		title: "Pita Pit" ,
-		description: "Food place"
-	},
-	{
-		lat:  -41.2898021,
-		lng:  174.7623089,
-		title: "Burger Liquer" ,
-		description: "Food place"
-	},
-	{
-		lat:  -41.2978421,
-		lng:  174.775696,
-		title: "Ekim Burgers" ,
-		description: "Food place"
-	},
-	
-
-
-
-];
 
 
 function init(){
@@ -106,8 +29,6 @@ function init(){
 		// Makes map draggable
 		draggable: true,
 
-
-		draggableCursor: "crosshair",
 		fullscreenControl: true,
 
 		backgroundColor: "#eee",
@@ -127,82 +48,61 @@ function init(){
 
 }
 
-google.maps.event.addDomListener(window, 'load', init);
-
-function addallMarkers(){
-	for (var i = 0; i < allMarkers.length; i++) {
-		marker = new google.maps.Marker({
-		position:{
-			lat: allMarkers[i].lat ,
-			lng: allMarkers[i].lng
-		},
-		map: map,
-		animation: google.maps.Animation.DROP,
-		icon: "images/icon.png",
-		title: allMarkers[i].title ,
-		description: allMarkers[i].description ,
-		draggable: true
-	})
-		markers.push(marker);
-		allInfoBox(marker);
-	}
-}
-
-
-
-
-
-
-	function toggleBounce(){
-		if (marker.getAnimation() === null) {
-			marker.setAnimation(google.maps.Animation.BOUNCE);
-		}else{
-			marker.setAnimation(null);
-		}
-
-
-	};
-
-
-function infoBox(){
-
-	infoBox = new google.maps.InfoWindow();
-	google.maps.event.addListener(marker, "click" , function(){
-		infoBox.setContent(marker.title);
-		infoBox.open(map, marker);
-
-	});
-}
-
-
-function allInfoBox(marker){
-
-	infoBox = new google.maps.InfoWindow();
-	google.maps.event.addListener(marker, "click" , function(){
-		infoBox.setContent(marker.title + "<br>" + marker.description);
-		infoBox.open(map, marker);
-		
-	currentMarker = marker;
-
-
-	});
-}
-
-
-
-
-
-
-
+google.maps.event.addDomListener(window, "load", init);
 
 
 
 
 $(document).ready(function(){
 
+	//this is where we apply opacity to the arrow
+	$(window).scroll( function(){
+
+	  //get scroll position
+	  var topWindow = $(window).scrollTop();
+	  //multipl by 1.5 so the arrow will become transparent half-way up the page
+	  var topWindow = topWindow * 1.5;
+	  
+	  //get height of window
+	  var windowHeight = $(window).height();
+	      
+	  //set position as percentage of how far the user has scrolled 
+	  var position = topWindow / windowHeight;
+	  //invert the percentage
+	  position = 1 - position;
+
+	  //define arrow opacity as based on how far up the page the user has scrolled
+	  //no scrolling = 1, half-way up the page = 0
+	  $(".arrow-wrap").css("opacity", position);
+
+	});
 
 
 
+
+
+
+	//Code stolen from css-tricks for smooth scrolling:
+	$(function() {
+	  $("a[href*=#]:not([href=#])").click(function() {
+	    if (location.pathname.replace(/^\//,"") == this.pathname.replace(/^\//,"") && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $("[name=" + this.hash.slice(1) +"]");
+	      if (target.length) {
+	        $("html,body").animate({
+	          scrollTop: target.offset().top
+	        }, 800);
+	        return false;
+	      }
+	    }
+	  });
+	});
+
+
+
+	$(".to_top").on("mousedown", function() {
+    $(this).toggleClass("clicked");
+});
 
 });
 
